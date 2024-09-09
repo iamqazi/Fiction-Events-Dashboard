0c8f91b0f5c54858200368cb058c4ac3a16aec8f
iamqazi <qaziobaid7897@gmail.com>
2024-09-09 13:54:33 -0700

    new changes

--- src/app/components/EventModal.js
+++ src/app/components/EventModal.js
@@ -39,18 +39,18 @@ export default function EventModal() {
       >
         <div className="flex flex-col md:flex-row justify-between border-b- p-4">
           <div className="md:w-2/3">
-            <h2 className="text-[22px] truncate max-w-full text-[#1E232A] font-[600] mb-2">
+            <h2 className="text-[18px] sm:text-[18px] md:text-[22px] lg:text-[22px] truncate max-w-full text-[#1E232A] font-[600] mb-2">
               {selectedEvent.title}
             </h2>
-            <p className="text-[#9AA8BD] text-[18px] font-inter mb-0 ">
+            <p className="text-[#9AA8BD] text-[14px] sm:text-[14px] md:text-[18px] lg:text-[18px] font-inter mb-0 ">
               Category:{" "}
-              <span className="uppercase text-[#9AA8BD] font-[600] text-[18px] font-inter">
+              <span className="uppercase text-[#9AA8BD] font-[600] text-[14px] sm:text-[14px] md:text-[18px] lg:text-[18px] font-inter">
                 {selectedEvent.category}
               </span>
             </p>
           </div>
           <div className="md:w-1/3 flex md:justify-end ">
-            <p className="text-[#797D8C] text-[16px] md:pt-4 pt-0 lg:pt-1 font-[600]">
+            <p className="text-[#797D8C] whitespace-nowrap text-[14px] sm:text-[14px] md:text-[16px] lg:text-[16px] md:pt-4 pt-0 lg:pt-1 font-[600]">
               {formatDateInYear(selectedEvent.start_local)},{" "}
               {formatTime(selectedEvent.start_local)}
             </p>
@@ -58,10 +58,10 @@ export default function EventModal() {
         </div>
         <hr className="border-t-[1px] py-2 border-[#C6CBD3]" />
 
-        <p className="text-[#1E232A] text-[20px] font-[400] pb-4 pl-4">
+        <p className="text-[#1E232A] text-[16px] sm:text-[18px] md:text-[20px] lg:text-[20px] font-[400] pb-4 px-4">
           Description:
           <br />
-          <span className="text-[#9E9E9E] text-[16px]">
+          <span className="text-[#9E9E9E] text-[14px] sm:text-[14px] md:text-[16px] lg:text-[16px] line-clamp-5 sm:line-clamp-5">
             {selectedEvent.description}
           </span>
         </p>
@@ -69,7 +69,7 @@ export default function EventModal() {
         <div className="items-center flex justify-center py-4">
           <div className="flex items-center gap-2">
             <Vector />
-            <span className="truncate max-w-[230px] text-[#797D8C] text-[20px] font-[400] font-inter">
+            <span className="truncate md:max-w-[230px] sm:max-w-[170px] max-w-[170px] lg:max-w-[230px] text-[#797D8C] text-[16px] sm:text-[18px] md:text-[20px] lg:text-[20px]  font-[400] font-inter">
               {selectedEvent.entities?.[0]?.formatted_address ||
                 selectedEvent.geo?.address?.formatted_address}
             </span>
--- src/app/ui/Empty.js
+++ src/app/ui/Empty.js
@@ -1,11 +1,12 @@
 import EmprtyImg from "../assets/empty.svg";
+
 export default function Empty() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen">
-      <div className="text-center">
-        <EmprtyImg className="w-[500px] mt-[-100px] h-[400px]" />
+    <div className="flex flex-col justify-center items-center min-h-screen px-4">
+      <div className="text-center mb-4">
+        <EmprtyImg className="w-[200px] md:w-[400px] lg:w-[500px] h-auto" />
       </div>
-      <div className=" text-center text-[16px] text-[#ED5656] font-semibold">
+      <div className="text-center text-[14px] sm:text-[16px] md:text-[18px] lg:text-[20px] text-[#ED5656] font-semibold">
         No Favorite Event has been added yet.
       </div>
     </div>
